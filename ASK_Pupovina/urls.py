ef08782159e43ee795ed6ecc6ceaf4ff50331dfc
Pavel196731 <pavzva200346@gmail.com>
2024-10-12 23:19:51 +0300

    init dajango

--- ASK_Pupovina/urls.py
+++ ASK_Pupovina/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ASK_Pupovina/', views.members, name='ASK_Pupovina'),
+    path('ASK_Pupovina/', views.index, name='ASK_Pupovina'),
+    path('ASK_Pupovina/stranica/', views.stranica, name='stranica')
 ]
--- ASK_Pupovina/views.py
+++ ASK_Pupovina/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def members(request):
-    return HttpResponse("Hello world!")
+def index(request):
+    return render(request, 'ASK_Pupovina/base.html')
+
+def stranica(request):
+    return render(request, 'ASK_Pupovina/about.html')
